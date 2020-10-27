456f581c23ad09a49aa240677bf4d185ecf36038
Sunil178 <sunilthakur178co@gmail.com>
2020-10-27 09:50:47 +0530

    Added closing button

--- app/src/main/java/com/example/proxyresest/MainActivity.java
+++ app/src/main/java/com/example/proxyresest/MainActivity.java
@@ -26,9 +26,14 @@ public class MainActivity extends AppCompatActivity {
             StrictMode.setThreadPolicy(policy);
             final PeriodicWorkRequest work = new PeriodicWorkRequest.Builder(MyWorker.class, 10, TimeUnit.SECONDS)
                     .setConstraints(Constraints.NONE)
+                    .addTag("FLIGHT_MODE_TOGGLE")
                     .build();
 //            final OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(MyWorker.class).build();
                 WorkManager.getInstance().enqueue(work);
         }
+    }
+    public void stopToggle(View view) {
+        WorkManager.getInstance().cancelAllWorkByTag("FLIGHT_MODE_TOGGLE");
+        Toast.makeText(getApplicationContext(),"Toggle Airplane Mode Stopped",Toast.LENGTH_SHORT).show();
     }
 }
